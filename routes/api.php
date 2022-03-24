24a2fee4c2d205f01081942ed5538400f6ac6959
MBakr <moh.bakr27@gmail.com>
2022-03-24 18:43:42 +0200

    heroku

--- routes/api.php
+++ routes/api.php
@@ -9,10 +9,11 @@ use App\Http\Controllers\UseCaseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+header("Cache-Control: no-cache, must-revalidate");
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
 header('Access-Control-Allow-Origin:  *');
-header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, PATCH, DELETE');
-header('Access-Control-Allow-Headers: Accept, Content-Type, X-Auth-Token, Origin, Authorization');
-
+header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
 # login & register & logout & approval
 
 
