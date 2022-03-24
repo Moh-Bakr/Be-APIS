81c498015fd256e15e954665669f64bb074b2dc5
MBakr <moh.bakr27@gmail.com>
2022-03-24 18:30:43 +0200

    heroku

--- config/cors.php
+++ config/cors.php
@@ -22,7 +22,8 @@ return [
     'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => ['*'],
-    'allowed_headers' => ['Authorization,Accept,Origin,DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range'],
+
+    'allowed_headers' => ['*'],
 
 
 //    'allowed_headers' => ['*'],
