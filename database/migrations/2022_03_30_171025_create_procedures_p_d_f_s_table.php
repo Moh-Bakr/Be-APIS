3adf20ea4ec438aea745373f0fdbeae0543bb50c
Bakr <moh.bakr27@gmail.com>
2022-03-31 16:36:23 +0200

    pdf response

--- database/migrations/2022_03_30_171025_create_procedures_p_d_f_s_table.php
+++ database/migrations/2022_03_30_171025_create_procedures_p_d_f_s_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
     {
         Schema::create('procedures_p_d_f_s', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
             $table->string('name');
             $table->string('file_path');
             $table->timestamps();
