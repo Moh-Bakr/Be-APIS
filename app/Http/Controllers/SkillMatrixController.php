ecf98b73035c26a992f76592fb63bd220c8dde83
Bakr <moh.bakr27@gmail.com>
2022-04-19 13:23:39 +0200

    skillmatrix

--- app/Http/Controllers/SkillMatrixController.php
+++ app/Http/Controllers/SkillMatrixController.php
@@ -10,7 +10,7 @@ class SkillMatrixController extends Controller
 {
     public function index()
     {
-        $SkillMatrix = SkillMatrix::orderBy('Level', 'ASC')->get();;
+        $SkillMatrix = SkillMatrix::orderBy('Level')->get();;
         return $SkillMatrix->groupBy('Category');
     }
 
