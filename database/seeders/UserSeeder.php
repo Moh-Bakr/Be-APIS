310918f8ec5e41efce22e9e8743af030d1de6f12
Bakr <moh.bakr27@gmail.com>
2022-05-07 01:58:43 +0200

    UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,19 +18,11 @@ class UserSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            [
-                'name' => 'admin',
-                'email' => 'admin@gmail.com',
-                'role' => 'admin',
-                'permission' => 'write',
-                'password' => bcrypt('admin@123'),
-            ],
-            [
-                'name' => 'test',
-                'email' => 'test@gmail.com',
-                'role' => 'test',
-                'permission' => 'read',
-                'password' => bcrypt('admin@123'),
-            ]]);
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'role' => 'admin',
+            'permission' => 'write',
+            'password' => bcrypt('admin@123'),
+        ]);
     }
 }
